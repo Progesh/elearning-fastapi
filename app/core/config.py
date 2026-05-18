52ad2e2e732789815d53b921037e9c7d535f0e74
Progesh Subba <progeshs@gmail.com>
2026-05-18 22:08:58 +0530

    modified config file to remove the depreicated pydantic settings file

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,11 @@
+from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 from urllib.parse import quote_plus
 
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env", extra="ignore")
+
     DB_HOST: str = "db"
     DB_PORT: int = 3306
     DB_USER: str = "root"
@@ -13,10 +16,6 @@ class Settings(BaseSettings):
     def DATABASE_URL(self) -> str:
         password = quote_plus(self.DB_PASSWORD)
         return f"mysql+pymysql://{self.DB_USER}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    class Config:
-        env_file = ".env"
-        extra = "ignore"
 
 
 settings = Settings()
